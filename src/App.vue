6234d483d2b7fbd05c66b49536381ff4fee2ec64
AudioStakes <daisuke.sato@enishi-tech.com>
2022-02-16 16:17:12 +0900

    Show a pressed key combination

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import CurrentKeyCombination from './components/CurrentKeyCombination.vue'
+
+const currentKeyboardEvent = ref(new KeyboardEvent(''))
+
+const onKeyDown = (e: KeyboardEvent) => {
+  if (e.repeat) return
+
+  console.log('onKeyDown', e.type, e)
+  e.preventDefault()
+  e.stopPropagation()
+
+  currentKeyboardEvent.value = e
+}
+
+const onKeyUp = (e: KeyboardEvent) => {
+  console.log('onKeyUp', e.type, e)
+  e.preventDefault()
+  e.stopPropagation()
+
+  currentKeyboardEvent.value = e
+}
+
+onMounted(() => {
+  document.addEventListener('keydown', onKeyDown)
+  document.addEventListener('keyup', onKeyUp)
+  document.onkeydown = () => false // https://stackoverflow.com/questions/37073277/how-to-disable-keyboard-shortcuts-completely-from-javascript
+})
+</script>
+
+<template>
+  <CurrentKeyCombination :currentKeyboardEvent="currentKeyboardEvent" />
+</template>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
