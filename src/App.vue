0436c208389a946787f3e491f3a539fc652ba143
AudioStakes <daisuke.sato@enishi-tech.com>
2022-03-02 15:59:29 +0900

    Show descriptions for unsupported browser and OS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted, computed } from 'vue'
 
+import Unsupported from './components/Unsupported.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import QuestionShow from './components/QuestionShow.vue'
@@ -149,10 +150,23 @@ onUnmounted(() => {
   window.removeEventListener('keyup', onKeyUp)
   window.onkeydown = null
 })
+
+const isUnsupportedBrowser = navigator.userAgent.indexOf('Chrome') === -1
+const isUnsupportedOs = navigator.userAgent.indexOf("Mac") === -1
+const isUnsupported = ref(isUnsupportedBrowser || isUnsupportedOs)
+const proceed = () => {
+  isUnsupported.value = false
+}
 </script>
 
 <template>
   <div class="font-mono antialiased text-slate-700 h-screen flex flex-col text-center">
+    <Unsupported
+      v-if="isUnsupported"
+      :isUnsupportedBrowser="isUnsupportedBrowser"
+      :isUnsupportedOs="isUnsupportedOs"
+      @proceed="proceed"
+    ></Unsupported>
     <Header />
     <main class="flex-auto flex flex-col justify-center">
       <template v-if="isAnsweredAllQuestions">
