8bba99aa95c8f1aa253795b95af5572e20acd6ab
AudioStakes <daisuke.sato@enishi-tech.com>
2022-03-11 15:35:13 +0900

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        'plugin:vue/vue3-recommended',
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module",
+        'extraFileExtensions': [
+            '.vue',
+        ],
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
