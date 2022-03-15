a7a0a2fb4fc30a2fc0132e2b353f426610c3f0d0
AudioStakes <daisuke.sato@enishi-tech.com>
2022-03-15 13:11:34 +0900

    Add an animation of removing a shortcut key

--- src/components/QuestionShow.vue
+++ src/components/QuestionShow.vue
@@ -2,13 +2,16 @@
 import GameKey from '@/stores/gameKey'
 import { injectStrict } from '@/utils'
 
-const { shortcut } = injectStrict(GameKey)
+const { state, shortcut } = injectStrict(GameKey)
 </script>
 
 <template>
   <div class="flex-initial h-32 flex flex-col">
     <span>{{ shortcut.app }} | {{ shortcut.category }}</span>
-    <h2 class="w-4/5 break-words text-3xl md:text-4xl font-bold m-auto">
+    <h2
+      class="w-4/5 break-words text-3xl md:text-4xl font-bold m-auto"
+      :class="state.isRemoveKeyPressed ? 'animate-[fadeOut_1000ms]' : ''"
+    >
       {{ shortcut.action }}
     </h2>
   </div>
