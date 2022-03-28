e053cef9b8f7b5d6628dc90280039b0dbfc96eb0
AudioStakes <daisuke.sato@enishi-tech.com>
2022-03-28 15:48:40 +0900

    Improve design of Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
-  <footer class="flex flex-col items-center max-h-10 text-xs m-1">
-    <a class="h-4 w-4 m-1" href="https://github.com/AudioStakes/shortype">
+  <footer
+    class="flex justify-center items-center space-x-2 text-xs py-2 bg-gray-100"
+  >
+    <a class="h-4 w-4" href="https://github.com/AudioStakes/shortype">
       <img src="../assets/GitHub-Mark-32px.png" />
     </a>
     <span>© 2022 Daisuke Sato</span>
